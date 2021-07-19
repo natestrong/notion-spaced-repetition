8032fe3c7b99a8d8a3b8d30b3ea3a84c4f1b940b
nstrong <nstrong@spanimation.com>
2021-07-18 18:49:17 -0700

    initial

---
+++ src/auth.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import path from 'path'
+import {Client} from '@notionhq/client/build/src'
+
+export interface IConfig {
+    repetitions:string[]
+    token_path: string
+    token: string
+}
+
+function spacedRepetitionConfigs():IConfig {
+    try {
+        const config: IConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'spaced-repetition.json'), 'utf8'));
+        config.token = fs.readFileSync(config.token_path, 'utf8');
+        return config;
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+export const configs = spacedRepetitionConfigs();
+
+export const notion = new Client({
+    auth:process.env.NOTION_TOKEN || configs.token,
+});
+
+notion.users.list()
